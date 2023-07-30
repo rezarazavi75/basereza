17941669efc8e1f76bb5894f47b01e2543e53011
rezarazavi75 <95575555+rezarazavi75@users.noreply.github.com>
2023-07-30 13:13:04 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x742e2d331b8bedf6e516e536814a87f033b79d2b";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
